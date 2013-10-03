1165ff88a1cb77d28510291924d0eaeeff812727
VaL <vd@nxc.no>
2013-10-03 09:28:15 +0200

    - Added read perform test

---
+++ tests/classes/nxcMemcachePerformanceReadTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author VaL <vd@nxc.no>
+ * @copyright Copyright (C) 2013 NXC AS.
+ * @license GNU GPL v2
+ * @package nxc_memcache
+ */
+
+class nxcMemcachePerformanceReadTest extends PHPUnit_Framework_TestCase
+{
+    const DIR = 'root/sub/dir/';
+    const FILES_COUNT = 200000;
+
+    public function testSetFiles()
+    {
+        $o = nxcMemcache::fetch( self::DIR . 'file_1' );
+        if ( $o )
+        {
+            //return;
+        }
+
+        for ( $i = 0; $i < self::FILES_COUNT; $i++ )
+        {
+            $o = new nxcMemcache( self::DIR . 'file_' . $i );
+            $this->assertTrue( $o->store( str_repeat( 'content', 1000 ) ) );
+        }
+    }
+
+    public function testGetFiles()
+    {
+        for ( $i = 0; $i < self::FILES_COUNT; $i++ )
+        {
+            $o = nxcMemcache::fetch( self::DIR . 'file_' . $i );
+            $this->assertTrue( $o instanceof nxcMemcache );
+            $this->assertEquals( str_repeat( 'content', 1000 ), $o->getContent() );
+        }
+    }
+}
+
+?>
